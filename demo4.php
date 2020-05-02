f055bc77d9b80a14abb66748200db6b3c56d0ad9
rgoud1 <64532015+rgoud1@users.noreply.github.com>
2020-05-02 17:48:24 +0530

    Update demo4.php

--- demo4.php
+++ demo4.php
@@ -1,4 +1,7 @@
 <?php
-echo 'this file is for creating conflict';
-echo 'This is from php strom to create a conflict';
+echo 'conflict 1';
+echo 'conflict 2';
+echo 'conflict 3';
+echo 'conflict 4';
+echo 'conflict 5';
 ?>
